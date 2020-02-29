f123a6e05c15232b1530026c5107ffba92b300c7
Nargiz Hasanova <nargiz7051@icloud.com>
2020-02-29 21:24:15 +0400

    calculator added

---
+++ Calculator.py
@@ -0,0 +1,14 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def calc(x,o,y):
+	if o=='*':
+		c=x*y
+	elif o=='+':
+		c=x+y
+	elif o=='-':
+		c=x-y
+	elif o=='/':
+		c=x/y
+	else:
+		c="Operatoru duzgun daxil edin:"
+	return c
