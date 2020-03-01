ea08838b9a3426773e3aa2e899adc1b761e5cfaf
Nargiz Hasanova <nargiz7051@icloud.com>
2020-03-01 15:25:11 +0400

    number is changed

--- Perfect number.py
+++ Perfect number.py
@@ -1,7 +1,7 @@
 Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 20:34:20) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> Number = int(input(" Enter any number: "))
- Enter any number: 13
+ Enter any number: 15
 >>> Sum = 0
 >>> for i in range(1, Number):
 	if (Number % i == 0):
